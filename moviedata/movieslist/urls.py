455ff477059ccaf71926b2ed8ead52c9b07331ef
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-06-25 23:25:37 +0530

    Migrations

--- moviedata/movieslist/urls.py
+++ moviedata/movieslist/urls.py
@@ -1,5 +1,6 @@
-from django.contrib import url 
+from django.urls import path 
+from . import views
 
 urlpatterns = [
-    
+    path('',views.getMovies,name="list")
 ]
--- moviedata/movieslist/views.py
+++ moviedata/movieslist/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from . models import Movie
+from django.http import JsonResponse
 
-# Create your views here.
+def getMovies(request):
+    movies = Movie.objects.all()
+    response = {
+        'movies':list(movies.values())
+    }
+    return JsonResponse(response)
