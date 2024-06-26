cb30619fbdbe726dda9fbbebdaaee82835c2bda2
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-06-26 23:18:07 +0530

    Crud

--- moviedata/movieslist/api/views.py
+++ moviedata/movieslist/api/views.py
@@ -4,14 +4,32 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def movieList(request):
-    movies = Movie.objects.all()
-    serialzer = MovieSerializer(movies,many=True)
-    return Response(serialzer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serialzer = MovieSerializer(movies,many=True)
+        return Response(serialzer.data)
+    elif request.method == 'POST':
+        serialzer = MovieSerializer(data = request.data)
+        if serialzer.is_valid():
+            serialzer.save()
+            return Response(serialzer.data)
+        else:
+            return Response(serialzer.errors)
 
-@api_view(['GET'])
+
+@api_view(['GET','PUT'])
 def individualMovieData(request,pk):
-    movies = Movie.objects.get(pk=pk)
-    serialzer = MovieSerializer(movies)
-    return Response(serialzer.data)
+      if request.method == 'GET':
+        movies = Movie.objects.get(pk=pk)
+        serialzer = MovieSerializer(movies)
+        return Response(serialzer.data)
+      elif request.method == 'PUT':
+          serialzer = MovieSerializer(data = request.data)
+          if serialzer.is_valid():
+            serialzer.save()
+            return Response(serialzer.data)
+          else:
+            return Response(serialzer.errors)
+          
